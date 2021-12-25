705497d93a36810ca6cd01ec927863608b744517
linhao <1334712251@qq.com>
2021-12-25 17:57:58 +0800

    提交结果

--- src/main/java/com/example/rabbit/one/Producer.java
+++ src/main/java/com/example/rabbit/one/Producer.java
@@ -10,7 +10,7 @@ public class Producer {
     public static void main(String[] args) throws Exception {
         //创建一个连接工厂
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("120.26.84.226/");
+        factory.setHost("120.26.84.226");
         factory.setUsername("guest");
         factory.setPassword("guest");
 
@@ -40,29 +40,5 @@ public class Producer {
          */
         channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
 
-
-        //channel 实现了自动 close 接口 自动关闭 不需要显示关闭
-//        try (Connection connection = factory.newConnection(); Channel connection.createChannel()){
-//
-//            /**
-//             * 生成一个队列
-//             * 1.队列名称
-//             * 2.队列里面的消息是否持久化 默认消息存储在内存中
-//             * 3.该队列是否只供一个消费者进行消费 是否进行共享 true 可以多个消费者消费
-//             * 4.是否自动删除 最后一个消费者端开连接以后 该队列是否自动删除 true 自动删除
-//             * 5.其他参数
-//             */
-//            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-//            String message = "hello world";
-//            /**
-//             * 发送一个消息
-//             * 1.发送到那个交换机
-//             * 2.路由的 key 是哪个
-//             * 3.其他的参数信息
-//             * 4.发送消息的消息体
-//             */
-//            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
-//            System.out.println("消息发送完毕");
-//        }
     }
 }
