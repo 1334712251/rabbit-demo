211b7f16b71ddc02fe7629fc565067764fb47833
linhao <1334712251@qq.com>
2021-12-30 10:53:25 +0800

    提交结果

--- src/main/java/com/example/rabbit/four/ConfirmMessage.java
+++ src/main/java/com/example/rabbit/four/ConfirmMessage.java
@@ -2,9 +2,14 @@ package com.example.rabbit.four;
 
 import com.example.rabbit.utils.RabbitMqUtils;
 import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.ConfirmCallback;
 
 import java.nio.charset.StandardCharsets;
+import java.util.NavigableSet;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentNavigableMap;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 
 /**
@@ -19,7 +24,8 @@ public class ConfirmMessage {
 
     public static void main(String[] args) throws Exception {
 //        ConfirmMessage.publishMessageIndividually();    //发布1000个单独确认消息,耗时31787ms
-        ConfirmMessage.publishMessageBatch();    //发布1000个批量确认消息,耗时450ms
+//        ConfirmMessage.publishMessageBatch();    //发布1000个批量确认消息,耗时450ms
+        ConfirmMessage.publishMessageAsync();    //发布1000个异步确认消息,耗时72ms
     }
 
     public static void publishMessageIndividually() throws Exception {
@@ -81,5 +87,90 @@ public class ConfirmMessage {
         }
         long end = System.currentTimeMillis();
         System.out.println("发布" + MESSAGE_COUNT + "个批量确认消息,耗时" + (end - begin) + "ms");
+    }
+
+
+    public static void publishMessageAsync() throws Exception {
+
+
+        Channel channel = RabbitMqUtils.getChannel();
+        String queueName = UUID.randomUUID().toString();
+        channel.queueDeclare(queueName, false, false, false, null);
+        //开启发布确认
+        channel.confirmSelect();
+        /**
+         * 线程安全有序的一个哈希表，适用于高并发的情况
+         * 1.轻松的将序号与消息进行关联
+         * 2.轻松批量删除条目 只要给到序列号
+         * 3.支持并发访问
+         */
+        ConcurrentSkipListMap<Long, String> outstandingConfirms = new ConcurrentSkipListMap<>();
+        /**
+         * 确认收到消息的一个回调
+         *
+         * 1.sequenceNumber:消息的序列号
+         * 2.multiple:是否为批量确认
+         *
+         * 1.消息序列号
+         * 2.true 可以确认小于等于当前序列号的消息
+         * false 确认当前序列号消息
+         */
+
+        AtomicInteger flag = new AtomicInteger();
+        ConfirmCallback ackCallback = (sequenceNumber, multiple) -> {
+//            System.out.println(flag.getAndIncrement());
+//            String message = outstandingConfirms.get(sequenceNumber);
+//            System.out.println("发布的消息" + message + "已被确认，序列号为" + sequenceNumber);
+            //删除已确认的信息，剩下的就是未确认的信息
+            //multiple:是否为批量确认
+            if (multiple) {
+//                ConcurrentNavigableMap<Long, String> confirmed = outstandingConfirms.headMap(sequenceNumber, true);
+                //拿到的消息是被确认的消息
+                //headMap方法通过sequenceNumber标记找到相关联的aLong序号，然后一起批量clear掉
+                ConcurrentNavigableMap<Long, String> confirmed = outstandingConfirms.headMap(sequenceNumber);
+                NavigableSet<Long> longs = confirmed.keySet();
+                for (Long aLong : longs) {
+                    String s = confirmed.get(aLong);
+//                    System.out.println("确认的消息" + s + "序号为" + aLong);
+                    System.out.println("标记为" + sequenceNumber + "序号为" + aLong);
+                }
+                //消除被批量确认的消息
+                confirmed.clear();
+            } else {
+                //不是批量确认消息,只清除当前序列号的消息
+                outstandingConfirms.remove(sequenceNumber);
+            }
+        };
+
+        /**
+         * 1.sequenceNumber:消息的序列号
+         * 2.multiple:是否为批量确认
+         */
+        ConfirmCallback nackCallback = (sequenceNumber, multiple) -> {
+            String message = outstandingConfirms.get(sequenceNumber);
+            System.out.println("发布的消息" + message + "未被确认，序列号为" + sequenceNumber);
+        };
+        /**
+         * 添加一个异步确认的监听器
+         * 1.确认收到消息的回调
+         * 2.未收到消息的回调
+         */
+//        channel.addConfirmListener(ackCallback, null);
+        channel.addConfirmListener(ackCallback, nackCallback);
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < MESSAGE_COUNT; i++) {
+            String message = "消息" + i;
+            /**
+             * channel.getNextPublishSeqNo()获取下一个消息的序列号
+             * 通过序列号与消息体进行一个关联
+             * 全部都是未确认的消息体
+             */
+            outstandingConfirms.put(channel.getNextPublishSeqNo(), message);
+            channel.basicPublish("", queueName, null, message.getBytes());
+            System.out.println("序列号为" + channel.getNextPublishSeqNo());
+            System.out.println("序列号----为" + channel.getNextPublishSeqNo());
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("发布" + MESSAGE_COUNT + "个异步确认消息,耗时" + (end - begin) + "ms");
     }
 }
